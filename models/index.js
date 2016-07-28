8542af5565c08a8e9a4b3eccaa8d5d88ffefe3ea
Vincent <vincentnewkirk@gmail.com>
2016-07-27 16:37:18 -1000

    more tinkering

--- models/index.js
+++ models/index.js
@@ -5,8 +5,14 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+
 var db        = {};
+
+if(env === 'production'){
+  console.log(process.env);
+} else {
+  var config    = require(__dirname + '/../config/config.json')[env];
+}
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
